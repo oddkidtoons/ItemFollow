91d912ccc3aba69fd6cb0d03fb1df265e05b6993
oddkidtoons <oddkidtoons@gmail.com>
2023-07-12 16:39:00 -0400

    follow script v1

---
+++ followNoTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PLAYERTWO.PlatformerProject;
+
+
+
+public class followNoTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public float MinModifier = 35;
+    public float MaxModifier = 45;
+ 
+
+    Vector3 _velocity = Vector3.zero;
+
+    bool _isFollowing = false;
+   
+
+    void Start()
+    {
+     player = FindObjectOfType<Player>().gameObject;
+        _isFollowing = true;
+    }
+
+ 
+
+
+        // public void StartFollowing()
+        // {
+        //     _isFollowing = true;
+        // }
+
+        void Update()
+    {
+        if (_isFollowing)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref _velocity, Time.deltaTime * Random.Range(MinModifier, MaxModifier));
+
+        }
+    }
+}
